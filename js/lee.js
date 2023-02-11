3693f90235cb2cb44f0d6ba78a2cc985c9c8a1d3
correcting the tescases <mohamedali.belkouri@smartqualitygate.com>
2023-02-11 22:19:18 +0100

    PLAYING

--- js/lee.js
+++ js/lee.js
@@ -1,10 +1,13 @@
 var http=require("http") ;
+var fs= require("fs") ;
+
 
 var server = http.createServer(function(req,res){
     res.writeHead(200,{
-        "Content-Type":"Text/plain" 
+        "Content-Type":"Text/html" 
     }) ; 
-    res.end("DALY_SERVER") ;
+    var stuff=fs.createReadStream(__dirname+'/jiren.html','utf8') ;
+    stuff.pipe(res) ;
 })
 
 server.listen(3000,'127.0.0.1') ;
